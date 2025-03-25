b219851a0ef5a6265bac798ad2cd4938b622cbc3
Travis5177 <163381634+FHJDAHK@users.noreply.github.com>
2025-03-26 06:29:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,8 @@ page = st.sidebar.radio("Go to", ["About Us", "Introduction", "Overview", "EDA",
 
 if page != "EDA":
     if "show_eda" in st.session_state:
-     st.session_state.show_eda = False
+        st.session_state.show_eda = False
+
 
 if page == "About Us":
     st.markdown("<p class='title'>👥 About Us</p>", unsafe_allow_html=True)
@@ -279,7 +280,7 @@ if page == "Overview":
 elif page == "EDA":
 
     if "show_eda" not in st.session_state:
-     st.session_state.show_eda = False
+        st.session_state.show_eda = False
 
      if not st.session_state.show_eda:
       st.markdown("""
