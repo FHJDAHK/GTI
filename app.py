0a81f178c184d82df5b0dd3b7d5aa97d6b0bfdc2
Travis5177 <163381634+FHJDAHK@users.noreply.github.com>
2025-03-26 08:13:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -337,7 +337,7 @@ elif page == "EDA":
            "Myanmar": "mm",
            "Syria": "sy"
            }
-        top_countries["Country+Flag"] = top_countries["Country"].map(
+               top_countries["Country+Flag"] = top_countries["Country"].map(
                 lambda x: f"<img src='https://flagcdn.com/w40/{flag_map[x]}.png' width='20'> {x}"
         )
             
