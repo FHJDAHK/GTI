83e65821b2797d0b54299a7e3549fd42df723b98
Travis5177 <163381634+FHJDAHK@users.noreply.github.com>
2025-03-26 09:56:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -304,8 +304,8 @@ elif page == "EDA":
         </p>
         """, unsafe_allow_html=True)
        
-      image = Image.open("11.webp")  
-      st.image(image, use_container_width=True)
+        image = Image.open("11.webp")  
+        st.image(image, use_container_width=True)
 
          
         if st.button("Explore Data 🔍"):
