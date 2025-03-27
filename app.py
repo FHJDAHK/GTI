e34519a2db9f25e5ff29b73a042c986ded18ce98
Travis5177 <163381634+FHJDAHK@users.noreply.github.com>
2025-03-27 09:58:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -415,7 +415,7 @@ elif page == "EDA":
              xaxis_title="Year",
              yaxis_title="People Count",
              yaxis_tickformat=",",  
-             template="simple_white"，
+             template="simple_white",
              xaxis=dict(
                showgrid=True,
                gridcolor='lightgrey',
